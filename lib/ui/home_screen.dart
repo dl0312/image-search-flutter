94fd4bd7c266b1289e622379c0034e13ddcd9716
Geon Lee <geon.lee@navercorp.com>
2022-09-04 02:30:58 +0900

    feat: image search

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,9 +1,36 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'dart:convert';
 
-class HomeScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_search/model/photo.dart';
+import 'package:image_search/ui/widget/photo_widget.dart';
+import "package:http/http.dart" as http;
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final _controller = TextEditingController();
+
+  List<Photo> _photos = [];
+
+  Future<List<Photo>> fetch(String query) async {
+    final response = await http.get(Uri.parse(
+        'https://pixabay.com/api/?key=29652629-7953ddc2e0dd24fe152bb732e&q=$query&image_type=photo'));
+
+    Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+    Iterable hits = jsonResponse['hits'];
+    return hits.map((e) => Photo.fromJson(e)).toList();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +49,7 @@ class HomeScreen extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: TextField(
+              controller: _controller,
               decoration: InputDecoration(
                 border: const OutlineInputBorder(
                   borderRadius: BorderRadius.all(
@@ -29,7 +57,12 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
                 suffixIcon: IconButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    final photos = await fetch(_controller.text);
+                    setState(() {
+                      _photos = photos;
+                    });
+                  },
                   icon: const Icon(Icons.search),
                 ),
               ),
@@ -37,24 +70,16 @@ class HomeScreen extends StatelessWidget {
           ),
           Expanded(
             child: GridView.builder(
-                padding: EdgeInsets.all(16),
-                itemCount: 10,
+                padding: const EdgeInsets.all(16),
+                itemCount: _photos.length,
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 2,
                   crossAxisSpacing: 16,
                   mainAxisSpacing: 16,
                 ),
                 itemBuilder: (buildContext, index) {
-                  return Container(
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(16)),
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: NetworkImage(
-                            'https://img.insight.co.kr/static/2020/12/18/700/img_20201218143147_2iw2d630.webp'),
-                      ),
-                    ),
-                  );
+                  final photo = _photos[index];
+                  return PhotoWidget(photo: photo);
                 }),
           ),
         ],
