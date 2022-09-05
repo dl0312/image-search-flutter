057f135dc2e6957fbe3a31581a121db11629c511
Geon Lee <geon.lee@navercorp.com>
2022-09-05 15:59:04 +0900

    feat: Stream을 활용하여 데이터 다루기

--- lib/data/photo_provider.dart
+++ lib/data/photo_provider.dart
@@ -1,16 +1,32 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:image_search/data/api.dart';
+import 'package:image_search/model/photo.dart';
 
 class PhotoProvider extends InheritedWidget {
   final PixabayApi api;
 
-  const PhotoProvider({super.key, required this.api, required super.child});
+  final _photoStreamController = StreamController<List<Photo>>()..add([]);
+  Stream<List<Photo>> get photoStream => _photoStreamController.stream;
 
+  PhotoProvider({
+    Key? key,
+    required this.api,
+    required Widget child,
+  }) : super(key: key, child: child);
+
+  // PhotoProvider({super.key, required this.api, required super.child});
   static PhotoProvider of(BuildContext context) {
     final PhotoProvider? result =
         context.dependOnInheritedWidgetOfExactType<PhotoProvider>();
     assert(result != null, 'No PixabayApi found in context');
     return result!;
+  }
+
+  Future<void> fetch(String query) async {
+    final result = await api.fetch(query);
+    _photoStreamController.add(result);
   }
 
   @override
